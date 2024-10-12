93f3f6d7de099797343cb90548479492f3500c8d
simran <simransingh956429@gmail.com>
2024-10-12 21:05:15 +0530

    if-else

---
+++ Conditional.java
@@ -0,0 +1,36 @@
+public class Conditional {
+    public static void main (String[]args)
+    {
+       // int x = 18;
+        
+//if---- else 
+
+//if(x>10)
+  // System.out.println("Hello"); // hello
+   //if(false)---it doesnt execute the block under
+   // if(x<10)----no output
+
+   //if (x>10 && x<20) // 11--20
+    // System.out.println("HELLO");//TRUE
+   //else
+     //System.out.println("bye");
+
+     int x = 5;
+     int y =7;
+     //greatest number
+
+     if (x>y) 
+     {//if more than one statement then bracket
+        System.out.println(x);
+        System.out.println("thank you");
+     }
+        else
+        System.out.println(y);
+        
+     }
+    
+
+
+    }
+    
+
