c0f7adf0f3e602f046a4dd9b6f33aeab18dee93e
simran <simransingh956429@gmail.com>
2024-10-15 23:06:43 +0530

    commit

---
+++ Switch.java
@@ -0,0 +1,33 @@
+public class Switch {
+    public static void main(String[] args) 
+    {
+        int n =1;
+
+        //---one way---//
+
+      //  if (n==1)
+            // System.out.println("Monday");
+          //else if (n==2)
+             // System.out.println("Tuesday");
+         // else if (n==3)
+              // System.out.println("Wednesday");
+        //else if (n==4)
+              //System.out.println("Thrusday");
+       // else if (n==5)
+              //System.out.println("Friday");
+         //else if (n==6)
+               //System.out.println("Saturday");
+          //else 
+                  //System.out.println("Sunday");
+
+
+                  /*----BETTER WAY----*/
+
+
+
+     
+
+
+    }
+    
+}
