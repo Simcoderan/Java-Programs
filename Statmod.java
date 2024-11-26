04ec65418040ac6590c02cc76707bfd0e9d2c04a
simran <simransingh956429@gmail.com>
2024-11-26 20:17:37 +0530

    loading a  class(jdbc)

---
+++ Statmod.java
@@ -0,0 +1,20 @@
+public class Statmod {
+
+    //we have special block to initialize the nstatic variable so that it doesnt need to initialize again again in constructors and objects
+
+
+//          static
+//          {
+
+//             name="Phone";
+//          }
+
+
+//class loader loads class first then obj
+//hence static class executes first
+//if  u dont create the obj it will not load the class
+
+//for this we have special class
+     //Class.forName(className:"Mobile");     forName is a method -and this whole is to load the class   
+    
+}
