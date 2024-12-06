730485db5e4089d6d5554eda62c2309394486120
simran <simransingh956429@gmail.com>
2024-12-06 07:19:16 +0530

    packages

---
+++ Pack.java
@@ -0,0 +1,5 @@
+//class belongs to a package
+//system is a class and it belongs to package-->>package java.lang.System [import java.lang.*]<<< (defaultly imported always)
+//to import all(*)- all the files not all the folders
+
+//when you r creating package and want to give access to others its name should be unique.(Libraries)
